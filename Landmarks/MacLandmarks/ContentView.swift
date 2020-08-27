e851f1e7a212add849da50f475b94c5f60ac08d9
Eitaro <e.yamatsuta49@gmail.com>
2020-08-27 19:35:27 +0900

    First commit

---
+++ Landmarks/MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by 山蔦栄太郎 on 2020/08/27.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
